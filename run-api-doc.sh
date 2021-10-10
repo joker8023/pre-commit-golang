a34bce42564d9d16911a47f1025e5ca87917dbed
Joker <joker@JokerdeMacBook-Pro.local>
2021-10-10 20:25:07 +0800

    modified:   run-api-doc.sh

--- run-api-doc.sh
+++ run-api-doc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-exec apidoc -i ./api  -o ./static/api-doc  -c ./docs --single &&sed -e '/\"time\"/d' static/api-doc/index.html > index.html&&mv index.html static/api-doc/
+exec  apidoc -i ./api  -o ./  -c ./docs --single &&sed -e '/\"time\"/d' .index.html > static/api-doc/index.html&&rm index.html
