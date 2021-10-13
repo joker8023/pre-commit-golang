bfdb8e866b70f6b0070af3ced6483310a8124762
lowmemory <jiangkai@bixin.cm>
2021-10-13 14:09:23 +0800

    modified:   run-api-doc.sh

--- run-api-doc.sh
+++ run-api-doc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-exec  apidoc -i ./api  -o ./  -c ./docs --single&&sed -e '/\"time\"/d' ./index.html > ./static/api-doc/index.html
+exec  apidoc -i ./api  -o ./static/api-doc  -c ./docs --single&&sed -in-place -e '/\"time\"/d' ./static/api-doc/index.html
 # exec  apidoc -i ./api  -o ./  -c ./docs --single &&sed -e '/\"time\"/d' ./index.html > static/api-doc/index.html&&rm index.html
